d36e66e6629811e2f202fb38a369f9e749b1d79d
Nicola 'tekNico' Larosa <nico@teknico.net>
2017-01-27 12:54:03 +0100

    Add tarball.nix file

---
+++ tarball.nix
@@ -0,0 +1,43 @@
+# Run like this:
+#   nix-build /path/to/this/directory/tarball.nix
+# ... and the files are produced in ./result/
+
+{ pkgs ? (import <nixpkgs> {})
+, name ? "snabb"
+, src ? ./.
+}:
+
+pkgs.stdenv.mkDerivation rec {
+  inherit name src;
+
+  buildInputs = with pkgs; [ git patchelf ];
+
+  postUnpack = ''
+    export DISTDIR="$out/${name}-`cd snabb; git describe --tags`"
+    mkdir -p $DISTDIR
+    cp -a $sourceRoot/* $DISTDIR
+  '';
+
+  installPhase = ''
+    mv src/snabb "$out"
+  '';
+
+  fixupPhase = ''
+    patchelf --shrink-rpath "$out/snabb"
+    patchelf --set-rpath /lib/x86_64-linux-gnu "$out/snabb"
+    patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 "$out/snabb"
+  '';
+
+  doDist = true;
+
+  distPhase = ''
+    cd "$out"
+    # The tarball will only contain the DISTDIR and the "snabb" binary.
+    export DISTDIR=`ls -I snabb`
+    # Remove a leftover stray binary. FIXME: should not happen.
+    rm "$DISTDIR/src/snabb"
+    tar Jcf $DISTDIR.tar.xz *
+# TODO: make tarball available through Hydra.
+#    cp -av $DISTDIR.tar.xz "$out"
+  '';
+}
