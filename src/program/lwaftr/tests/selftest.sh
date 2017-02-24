64bd74cc5959e76a847557262fda3a8fb8814221
Nicola 'tekNico' Larosa <nico@teknico.net>
2017-02-24 13:24:57 +0100

    Reimplement the 'snabb lwaftr bench' test in Python

---
+++ src/program/lwaftr/tests/selftest.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Make it work from wherever this script is called, and let tests know.
+export TESTS_DIR=`dirname "$0"`
+
+# Entry point for Python tests.
+#
+# Start discovery from this script's directory, the root of the "tests" subtree.
+# Find unittests in all Python files ending with "_test.py".
+# Show values of local variables when there are errors.
+# List all executed tests, don't show just dots.
+python3 -m unittest discover \
+    export --start-directory "${TESTS_DIR}" \
+    --pattern "*_test.py" \
+    --locals \
+    --verbose
---
+++ src/program/lwaftr/tests/subcommands/branch_test.py
@@ -0,0 +1,53 @@
+"""
+Test the "snabb lwaftr bench" subcommand. Does not need NIC cards.
+"""
+
+import os
+from pathlib import Path
+import unittest
+
+from lib import sh
+
+
+# Commands run under "sudo" run as root. The root's user PATH should not
+# include "." (the current directory) for security reasons. If this is the
+# case, when we run tests from the "src" directory (where the "snabb"
+# executable is), the "snabb" executable will not be found by relative paths.
+# Therefore we make all paths absolute.
+TESTS_DIR = Path(os.environ['TESTS_DIR']).resolve()
+DATA_DIR = TESTS_DIR / 'data'
+BENCHDATA_DIR = TESTS_DIR / 'benchdata'
+SNABB_CMD = TESTS_DIR.parents[2] / 'snabb'
+BENCHMARK_FILENAME = 'benchtest.csv'
+BENCHMARK_PATH = TESTS_DIR / BENCHMARK_FILENAME
+
+
+class TestBenchSubcommand(unittest.TestCase):
+
+    test_args = (
+        SNABB_CMD, 'lwaftr', 'bench',
+        '--duration', '0.1',
+        '--bench-file', BENCHMARK_FILENAME,
+        DATA_DIR / 'icmp_on_fail.conf',
+        BENCHDATA_DIR / 'ipv4-0550.pcap',
+        BENCHDATA_DIR / 'ipv6-0550.pcap',
+    )
+
+    def run_bench_test(self, args):
+        output = sh.sudo(*args)
+        self.assertEqual(output.exit_code, 0)
+        self.assertTrue(BENCHMARK_PATH.is_file(),
+            'Cannot find {}'.format(BENCHMARK_FILENAME))
+        BENCHMARK_PATH.unlink()
+
+    def test_standard(self):
+        self.run_bench_test(self.test_args)
+
+    def test_reconfigurable(self):
+        reconf_args = list(self.test_args)
+        reconf_args.insert(3, '--reconfigurable')
+        self.run_bench_test(reconf_args)
+
+
+if __name__ == '__main__':
+    unittest.main()
