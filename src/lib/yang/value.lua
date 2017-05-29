36f33647b96c12bdc060627057d4a2c3726df0fa
Diego Pino Garcia <dpino@igalia.com>
2017-05-29 17:54:49 +0200

    Implement leafref

--- src/lib/yang/value.lua
+++ src/lib/yang/value.lua
@@ -96,7 +96,14 @@ function types.identityref.tostring(val)
 end
 
 types['instance-identifier'] = unimplemented('instance-identifier')
-leafref = unimplemented('leafref')
+
+types.leafref = {}
+function types.leafref.parse(str, what)
+   return assert(str, 'missing value for '..what)
+end
+function types.leafref.toleafref(val)
+   return val
+end
 
 types.string = {}
 function types.string.parse(str, what)
