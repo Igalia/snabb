eb3dd1dce1f2e780700da75472c72c128f6a600d
Nicola 'tekNico' Larosa <nico@teknico.net>
2017-07-05 18:39:25 +0200

    Stop the lwaftr monitor test from hanging, make it fail instead

--- src/program/lwaftr/tests/selftest.sh
+++ src/program/lwaftr/tests/selftest.sh
@@ -6,11 +6,11 @@
 export TESTS_DIR=`dirname "$0"`
 export PYTHONPATH=${TESTS_DIR}
 
-# Only run tests in the passed subdirectory of $TESTS_DIR.
+# Only run tests in the chosen test file (without the _test.py suffix).
 if [[ -n $1 ]]; then
-    START_DIR=${TESTS_DIR}/$1/
+    TEST_WHAT=$1
 else
-    START_DIR=${TESTS_DIR}
+    TEST_WHAT="*"
 fi
 
 # Start discovery from this script's directory, the root of the "tests" subtree,
@@ -18,6 +18,6 @@ fi
 # Look for unittests in all files whose name ends with "_test.py".
 # List all executed tests, don't show just dots.
 python3 -m unittest discover \
-    --start-directory "${START_DIR}" \
-    --pattern "*_test.py" \
+    --start-directory "${TESTS_DIR}" \
+    --pattern "${TEST_WHAT}_test.py" \
     --verbose
