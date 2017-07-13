03ecb2fcce76de9165b5d169efa3bd1be3cf244b
Diego Pino Garcia <dpino@igalia.com>
2017-07-13 16:43:49 +0000

    Load alarm inventory

--- src/lib/yang/alarms.lua
+++ src/lib/yang/alarms.lua
@@ -2,15 +2,9 @@ module(..., package.seeall)
 
 local S = require("syscall")
 
-local function set (t)
-   local ret = {}
-   for _, k in ipairs(t) do ret[k] = true end
-   return ret
-end
-
 local config = {
    control = nil,
-} 
+}
 
 local state = {
    alarm_inventory = nil,
@@ -19,90 +13,96 @@ local state = {
    shelved_alarms = nil,
 }
 
+-- Static alarm_inventory list.
+local alarm_inventory_table = {
+   { alarm_type_id='arp-resolution', alarm_type_qualifier='', resource={'external-interface'}, has_clear=true, description=''},
+   { alarm_type_id='ndp-resolution', alarm_type_qualifier='', resource={'internal-interface'}, has_clear=true, description=''},
+}
+
+local function add_row_to_alarm_inventory (alarm_inventory, row)
+   if not alarm_inventory.alarm_type then alarm_inventory.alarm_type = {} end
+   local alarm_type = alarm_inventory.alarm_type
+   local key = {alarm_type_id=row.alarm_type_id,alarm_type_qualifier=row.alarm_type_qualifier}
+   if not alarm_type[key] then
+      local value = {
+         alarm_type_id=row.alarm_type_id,
+         alarm_type_qualifier=row.alarm_type_qualifier,
+         resource=row.resource,
+         has_clear=row.has_clear,
+         description=row.description,
+      }
+      alarm_type[key] = value
+   end
+end
+
+-- The alarms inventory is always initialized with a preset of alarm inventory
+-- The reason is that it doesn't really matter is an user defines new alarm
+-- types in a configuration.  The system will only rises a predefined set of
+-- alarms.  For the same reason, there's an static list of alarms.
+local function load_alarm_inventory (alarms, t)
+   alarms.alarm_inventory = {}
+   local alarm_inventory = alarms.alarm_inventory
+   for _, row in ipairs(t) do
+      add_row_to_alarm_inventory(alarm_inventory, row)
+   end
+end
+
+local function table_size (t)
+   local ret = 0
+   for _ in pairs(t) do ret = ret + 1 end
+   return ret
+end
+
+local function init_alarm_inventory (alarm_inventory)
+   state.alarm_inventory = alarm_inventory
+   load_alarm_inventory(state.alarm_inventory, alarm_inventory_table)
+end
+
 function init (current_configuration)
    local softwire_config = current_configuration.softwire_config
    local softwire_state = current_configuration.softwire_state
    config.control = softwire_config.alarms.control
-   state.alarm_inventory = softwire_state.alarms.alarm_inventory
+   init_alarm_inventory(softwire_state.alarms.alarm_inventory)
    state.summary = softwire_state.alarms.summary
    state.alarm_list = softwire_state.alarms.alarm_list
    state.shelved_alarms = softwire_state.alarms.shelved_alarms
 end
 
--- Statically create alarm_inventory and alarm_list.
-local alarm_inventory = {
-
-}
-
--- Summary should be read-only.
-
--- Shelved-alarms
-
--- Supported common-alarm-parameters and resource-alarm-parameters.
-local alarm_params = set{'alt_resource', 'impacted_resource',
-   'root_cause_resource', 'is_cleared', 'last_changed', 'perceived_severity',
-   'alarm_text'}
-
-local alarms = {
-   list = {},
-   shelved = {},
-   summary = {},
-}
-
-Alarm = {}
-
-function Alarm.new (key, params)
-   assert(key.resource and key.alarm_type_id and key.alarm_type_qualifier,
-      'Not valid alarm key')
-   local o = {}
-   for k,v in pairs(key) do
-      o[k] = v
+-- Helper function for debugging purposes.
+local function show_alarm_inventory (alarm_inventory)
+   local function pp(t)
+      for k,v in pairs(t) do
+         if type(v) == 'table' then
+            io.stdout:write(k..':\n  ')
+            pp(v)
+         else
+            if type(v) == 'boolean' then 
+               v = v and 'true' or 'false'
+               elseif type(v) == 'nil' then
+                  v = 'nil'
+               end
+               print(k..': '..v)
+            end
+         end
+      end
+      local alarm_type = alarm_inventory.alarm_type or {}
+      for k,v in pairs(alarm_inventory.alarm_type) do
+         pp(v)
+         print("--")
+      end
    end
-   for k,v in pairs(params or {}) do
-      assert(alarm_params[k], 'Not valid alarm param: '..k)
-      o[k] = v
-   end
-   return setmetatable(o, {__index = Alarm})
-end
 
 -- Q: What's an alarm?
 -- A: The data associated with an alarm is the data specified in the yang schema.
 
 -- to be called by the leader.
 function set_alarm (key, args)
-   -- alarms.list[key] = key
-   config.control.notify_status_changes = false
    print('set_alarm')
-   for k, v in pairs(args) do
-      print(k, v)
-   end
-   --[[
-   local alarm = Alarm.new(key, params)
-   table.insert(alarms.list, alarm)
-   --]]
 end
 
 -- to be called by the leader.
 function clear_alarm (key, args)
    print('clear_alarm')
-   for k, v in pairs(args) do
-      print(k, v)
-   end
-end
-
-local function equals_key (key1, key2)
-   return key1.resource == key2.resource and
-          key1.alarm_type_id == key2.alarm_type_id and
-          key1.alarm_type_qualifier == key2.alarm_type_qualifier
-end
-
--- does it indicate which schema name.
-function get_alarm (key)
-   for _, alarm in ipairs(alarms.list) do
-      if equals_key(alarm, key) then
-         return alarm
-      end
-   end
 end
 
 -- to be called by the config leader.
@@ -124,20 +124,14 @@ function purge_alarms ()
    return 0
 end
 
---[[
-function selftest ()
-   local key = {
-      resource = 'resource1',
-      alarm_type_id = 'alarm_type1',
-      alarm_type_qualifier = 'alarm_type_qualifier1',
-   }
-   set_alarm(key)
-   assert(get_alarm(key))
-end
---]]
-
 function selftest ()
    -- Reads lwaftr.conf file.
+   local capabilities = {
+      ['ietf-softwire']={feature={'binding', 'br'}},
+      ['ietf-alarms']={feature={'operator-actions', 'alarm-shelving', 'alarm-history'}},
+   }
+   require('lib.yang.schema').set_default_capabilities(capabilities)
+
    local data = require("lib.yang.data")
    local conf = [[
       softwire-config {
@@ -188,13 +182,22 @@ function selftest ()
       }
       softwire-state {
          alarms {
-
+            summary {
+               alarm-summary {
+                 severity minor;
+                 total 32;
+                 cleared 100;
+                 cleared-not-closed 10;
+               }
+            }
          }
       }
    ]]
    local conf = data.load_data_for_schema_by_name('snabb-softwire-v2', conf)
    assert(conf.softwire_config.alarms.control.notify_status_changes)
 
-   -- Do stuff with the alarm related containers.
-end 
-
+   -- Test load alarm inventory.
+   local alarms = conf.softwire_state.alarms
+   load_alarm_inventory(alarms, alarm_inventory_table)
+   assert(table_size(alarms.alarm_inventory.alarm_type) == 2)
+end
